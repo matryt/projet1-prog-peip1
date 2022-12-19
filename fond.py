d47da5c82395de85fa87be28f39fbccf10941ef7
LoicL832 <loiclamour0@gmail.com>
2022-12-20 00:32:40 +0100

    changement tronc en rectangle

--- fond.py
+++ fond.py
@@ -31,16 +31,18 @@ def rectangle(l,x,y,couleur):
     up()
     goto(x,y)
     down()
+    begin_fill()
     color(couleur)
     forward(l)
     left(90)
-    forward(2*l)
+    forward(3/2*l)
     left(90)
     forward(l)
     left(90)
-    forward(2*l)
+    forward(3/2*l)
+    end_fill()
  
-rectangle(50,0,0,'brown')
+
 
         
 
@@ -49,18 +51,20 @@ rectangle(50,0,0,'brown')
 def sapin(l,x,y):
     hideturtle()
     begin_fill()
-    dessinePolygone(4,l,x,y,'brown')
+    rectangle(l,x,y,'brown')
     end_fill()
+    setheading(0)
     begin_fill()
     up()
-    dessineTriangle(2*l,x-l/2,l,'green')
+    dessineTriangle(2*l,x-l/2,3/2*l,'green')
     end_fill()
     begin_fill()
-    dessineTriangle((3/2)*l,x-l/3,l+sqrt(l**2/4 + l**2)-1,'green')
+    dessineTriangle((3/2)*l,x-l/3,3/2*l+sqrt(l**2/4 + l**2)-1,'green')
     end_fill()
     begin_fill()
-    dessineTriangle(l,x-l/4+10,l+sqrt(l**2/4 + l**2)-1+l,'green')
+    dessineTriangle(l,x-l/4+10,3/2*l+sqrt(l**2/4 + l**2)-1+l,'green')
     end_fill()
+
 sapin(60,0,0)
 
     
